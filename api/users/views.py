7820c0e2c620b79a5c9629e366c71b26d34ef193
DOUMBOUYA FODE <47141813+fodedoumbouya@users.noreply.github.com>
2023-04-29 17:02:01 +0200

    .....

--- api/users/views.py
+++ api/users/views.py
@@ -1,8 +1,11 @@
 
+from utils.constant import *
 from . import users_bp
 from flask import jsonify, request
 from flask_bcrypt import Bcrypt
-from config.config import Config
+from config.config import Config, db_name
+from utils.sqlReturn import *
+
 
 bcrypt = Bcrypt()
 
@@ -22,14 +25,16 @@ def create_user():
 
     # Vérifiez que toutes les données sont présentes
     if not name or not email or not login or not password:
-        return jsonify({'error': 'Tous les champs sont requis'}), 400
+        return resquestErrorResponse(msg="Tous les champs sont requis", cd=400)
+    # jsonify({'error': 'Tous les champs sont requis'}), 400
 
     # Vérifiez si l'utilisateur existe déjà dans la base de données
     cursor = db.cursor()
     cursor.execute("SELECT * FROM utilisateur WHERE login=%s", (login,))
     user = cursor.fetchone()
     if user:
-        return jsonify({'error': 'Cet utilisateur existe déjà'}), 400
+        return resquestErrorResponse(msg="Cet utilisateur existe déjà", cd=400)
+    # jsonify({'error': ''}), 400
 
     # Hachez le mot de passe de l'utilisateur
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
@@ -40,16 +45,23 @@ def create_user():
     db.commit()
 
     # Retournez les données de l'utilisateur créé
-    return jsonify({"status": "OK"}), 201
+    sql = "SELECT * from {0}.utilisateur where email = {1} and pwd ='{2}'".format(
+        db_name, email, hashed_password)
+    resp = requestSelect(sql=sql)
+    return resp
 
 
 # Route pour lister tous les utilisateurs
 @users_bp.route('/', methods=['GET'])
 def list_users():
     # Récupérez tous les utilisateurs de la base de données
-    cursor = db.cursor()
-    cursor.execute("SELECT nom, email, login FROM utilisateur")
-    utilisateur = cursor.fetchall()
+    # cursor = db.cursor()
+    # cursor.execute("SELECT nom, email, login FROM utilisateur")
+    # utilisateur = cursor.fetchall()
 
-    # Retournez les données des utilisateurs sous forme de JSON
-    return jsonify(utilisateur)
+    # # Retournez les données des utilisateurs sous forme de JSON
+    # return jsonify(utilisateur)
+    sql = "SELECT nom, email, login from {0}.utilisateur".format(
+        db_name)
+    resp = requestSelect(sql=sql)
+    return resp
