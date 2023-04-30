ab27f868d8ef49fa2261132bd3cb0f9c294b068f
Edouard1er <edouardchevenslove98@gmail.com>
2023-04-30 18:15:17 +0300

    Update login feature and create logout feauture

--- api/auth/views.py
+++ api/auth/views.py
@@ -3,6 +3,9 @@ from flask import jsonify, request
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from utils.sqlReturn import getOnlyData
 from flask_bcrypt import Bcrypt
+from config.config import db_name
+import flask
+from utils.sqlReturn import update, constant
 
 bcrypt = Bcrypt()
 
@@ -15,10 +18,38 @@ def login():
         return jsonify({"msg": "Nom d'utilisateur manquant"}), 400
     if not password:
         return jsonify({"msg": "Mot de passe manquant"}), 400
-    sql = "SELECT pwd from utilisateur WHERE login = '{0}'".format(login)
+    sql = "SELECT u.id, u.email, u.name, u.imageUrl, u.filiere, u.promotion, u.pwd from utilisateur u WHERE login = '{0}'".format(login)
     response = getOnlyData(sql)
     if(len(response) == 0 or not bcrypt.check_password_hash(response[0]["pwd"], password)):
         return jsonify({"msg": "Nom d'utilisateur ou mot de passe incorrect"}), 401
     # Générer un jeton JWT avec l'identité de l'utilisateur
     access_token = create_access_token(identity=login)
-    return jsonify(access_token=access_token), 200
+    sql = "UPDATE {0}.utilisateur SET online='1' WHERE login = '{1}'".format(
+        db_name, login)
+    update(sql=sql)
+    
+    retour = {
+        "utilisateur" : {
+            "id" : response[0]["id"],
+            "email" : response[0]["email"],
+            "name" : response[0]["name"],
+            "imageUrl" : response[0]["imageUrl"],
+            "filiere" : response[0]["filiere"],
+            "promotion" : response[0]["promotion"],
+            "login" : login
+        },
+        "access_token" : access_token
+    }   
+    return jsonify(retour), 200
+
+@auth_bp.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    login = get_jwt_identity()
+    try:
+        sql = "UPDATE {0}.utilisateur SET online='0' WHERE login = '{1}'".format(
+            db_name, login)
+        resp = update(sql=sql)
+        return resp
+    except Exception as e:
+        return constant.resquestErrorResponse(e)
