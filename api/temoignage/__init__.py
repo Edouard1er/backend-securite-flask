70e9b894f4df713a59fee4761ae1d25a85e0a1b1
Edouard1er <edouardchevenslove98@gmail.com>
2023-05-01 13:09:22 +0300

    CRUD temoignages

---
+++ api/temoignage/__init__.py
@@ -0,0 +1,6 @@
+
+from flask import Blueprint
+
+temoignage_bp = Blueprint('temoignage_bp', __name__)
+
+from . import views
---
+++ api/temoignage/views.py
@@ -0,0 +1,75 @@
+import flask
+from . import temoignage_bp
+from flask import jsonify, request, abort
+from config.config import db_name
+from flask_jwt_extended import jwt_required
+from utils.sqlReturn import *
+
+
+@temoignage_bp.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@jwt_required()
+def get_message_forum():
+    if request.method == 'GET':
+        id = flask.request.values.get('id')
+        try:
+            if id == None :
+                sql = "SELECT t.*, u.name, u.imageUrl, u.filiere, u.promotion from {0}.temoignage t INNER JOIN {0}.utilisateur u ON (t.id_user=u.id) WHERE t.statut='1'".format(db_name)
+            else:
+                sql = "SELECT t.*, u.name, u.imageUrl, u.filiere, u.promotion from {0}.temoignage t INNER JOIN {0}.utilisateur u ON (t.id_user=u.id) WHERE t.id = {1}".format(
+                    db_name, id)
+            print(sql)
+            resp = requestSelect(sql=sql)
+            return resp
+        except Exception as e:
+            return constant.resquestErrorResponse(e)
+    if request.method == 'POST':
+        if not request.json:
+            abort(400)
+        try:
+            _json = request.json
+            text = _json["text"]
+            id_frm = _json["rating"]
+            id_user = _json['id_user']
+            sql = "INSERT INTO {0}.temoignage (text, rating, id_user) VALUES(%s,%s,%s)".format(
+                db_name)
+            data = [text, id_frm, id_user]
+            resp = insert(sql=sql, data=data)
+            return resp
+        except Exception as e:
+            print(e)
+            return constant.resquestErrorResponse(e)
+
+    if request.method == 'PUT':
+        if not request.json:
+            abort(400)
+        try:
+            _json = request.json
+            id_temoignage = _json['id']
+            text = _json['text']
+            rating = _json['rating']
+            sql = "UPDATE {0}.temoignage SET text = '{1}', rating={3} where id = {2}".format(
+                db_name, text, id_temoignage, rating)
+            resp = update(sql)
+            return resp
+        except Exception as e:
+            return constant.resquestErrorResponse(e)
+
+    if request.method == 'DELETE':
+        try:
+            id = flask.request.values.get('id')
+            if id == None:
+                message = {
+                    'status': 200,
+                    'message': 'Please add ID',
+                }
+                resp = jsonify(message)
+                resp.status_code = 200
+                return resp
+            else:
+                sql = "UPDATE {0}.temoignage SET statut='0' WHERE id = {1}".format(
+                    db_name, id)
+                resp = delete(sql=sql)
+                return resp
+        except Exception as e:
+            print(e)
+            return constant.resquestErrorResponse(e)
